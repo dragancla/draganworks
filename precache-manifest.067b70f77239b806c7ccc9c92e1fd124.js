66745586f81528d153940421856fa20d15f6faa7
Anton Dragan <anton.dragan@hallon.se>
2022-11-27 21:33:41 +0100

    Updates

---
+++ precache-manifest.067b70f77239b806c7ccc9c92e1fd124.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4de3cc742393b7773fed8695c6305de5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "517e7209b0a03e0bac99",
+    "url": "/static/css/main.e8c24495.chunk.css"
+  },
+  {
+    "revision": "2cd88fc406f68f6dcdc1",
+    "url": "/static/js/2.aec7b878.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.aec7b878.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "517e7209b0a03e0bac99",
+    "url": "/static/js/main.df966261.chunk.js"
+  },
+  {
+    "revision": "92851905dda0221ebc80",
+    "url": "/static/js/runtime-main.608e8d6b.js"
+  },
+  {
+    "revision": "679d01a81f58f903739a0d434de09c65",
+    "url": "/static/media/resume.679d01a8.pdf"
+  }
+]);
