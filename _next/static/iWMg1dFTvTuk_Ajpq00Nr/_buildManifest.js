d80d2f96ad67ff176d89c5ced43dffaf161ea606
dragancla <dragancla@gmail.com>
2023-09-03 12:20:51 +0000

    Deploying to gh-pages from @ dragancla/draganworks@e4b351e0196fc833f46eb6170a4bc9b0ad7d8e5b 🚀

--- _next/static/iWMg1dFTvTuk_Ajpq00Nr/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/320-dd5d3286328ebe9a.js","static/chunks/pages/index-38c24a8524f4d21f.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
