946c205f5885c749da4c458d1ad86378ff85afe3
dragancla <anton@dragan.works>
2025-03-29 22:55:52 +0000

    Deploying to gh-pages from @ dragancla/draganworks@6830b2910618acaca6262676c8c101a609355ffb 🚀

---
+++ _next/static/LBStyLNL4dgY75phS73_n/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/303-fc8a0f4d662abd2c.js","static/chunks/pages/index-925a33b36f1f1d3b.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
